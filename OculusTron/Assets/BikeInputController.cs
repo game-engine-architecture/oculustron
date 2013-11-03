cb94ab6f630b20928dc9ad898ebfe3819bd05485
Tom <tom@Toms-Mac-Pro.local>
2013-11-03 20:32:57 +0100

    initial commit

---
+++ OculusTron/Assets/BikeInputController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeInputController : MonoBehaviour {
+	
+	public CharacterController characterController;
+	Transform modelTransform;
+	public float movementSpeed = 1;
+	public float rotationSpeed = 130;
+	
+	// Use this for initialization
+	void Start () {
+		this.modelTransform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaT = Time.deltaTime;
+		float hori = Input.GetAxis("Horizontal");
+		if (Input.GetKeyDown ("left")){
+			modelTransform.Rotate(new Vector3(0, -90, 0));	
+		} else if(Input.GetKeyDown ("right")){
+			modelTransform.Rotate(new Vector3(0, 90, 0));	
+		}
+		
+		
+		//rotationSpeed*hori
+		float vert = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(0,0,0);
+		movement += this.modelTransform.forward * vert * movementSpeed;
+		//deltaT*movementSpeed
+		characterController.Move(movement);
+	}
+}
---
+++ OculusTron/Assets/Follower.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+	
+	public Transform followedTransform;
+	public Transform cameraTransform;
+	Transform trans;
+	public float smoothness = 0.9f;
+	public float rotationSmoothness = 2f;
+	// Use this for initialization
+	void Start () {
+		trans = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaT = Time.deltaTime;
+		cameraTransform.rotation.SetFromToRotation(trans.position, followedTransform.forward);
+		//Vector3 rot = (cameraTransform.rotation.eulerAngles - followedTransform.rotation.eulerAngles) * rotationSmoothness * deltaT;
+		Vector3 rot = followedTransform.rotation.eulerAngles;
+		//Quaternion.
+		//Vector3 camoffset = trans.position;
+		//cameraTransform.rotation = Quaternion.Euler(camoffset * rot);
+		//cameraTransform.Rotate(rot);
+		
+		Vector3 newcamvec = (trans.position + followedTransform.position - cameraTransform.position) * smoothness * deltaT;
+		cameraTransform.position += newcamvec;
+		//trans.position
+		
+		
+		
+		
+		
+	}
+}
