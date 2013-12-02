c82a35ad1a96c8a16f2490eda8c631c2c484ba90
Philipp Liepert <philipp@phillsoft.de>
2013-12-02 20:45:53 +0100

    Menu added...

---
+++ OculusTron/Assets/MenuElementControll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuElementControll : MonoBehaviour {
+	
+	void OnMouseEnter() {
+		renderer.material.color = new Color(0.6f,0.6f,0.6f,0.6f);
+ 
+    }
+	
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+       
+    }
+	
+	void OnMouseUp(){
+		//Application.Quit();	
+		 //Debug.Log(" click");
+		
+		Application.LoadLevel(1);
+	}
+		
+}
+
+
---
+++ OculusTron/Assets/MenuInputScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInputScript : MonoBehaviour {
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("left")){
+			this.transform.Rotate(0,10,0);
+		}
+	}
+}
