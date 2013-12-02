6e361a85f74ee079d546f8c2fcfd21d8eeb46bee
Philipp Liepert <philipp@phillsoft.de>
2013-12-02 14:12:18 +0100

    Added Hotkey for OVR Support

---
+++ OculusTron/Assets/BikeHotkeyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeHotkeyController : MonoBehaviour {
+	
+	Component oculusCam = null;
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+		checkHotKey();
+	}
+	
+	void checkHotKey(){
+		if(Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.O)){
+			
+			
+			if (oculusCam==null){
+				Debug.Log("add ovr controller");
+				//oculusCam = this.gameObject.AddComponent("OVRCameraController");
+				oculusCam = this.gameObject.AddComponent("OVRPlayerController");
+				//Debug.Log("add ovr controller");
+			}else{
+				Debug.Log("remove ovr controller");
+				Destroy(oculusCam);
+				oculusCam = null;	
+			}
+			
+			
+		}	
+	}
+}
