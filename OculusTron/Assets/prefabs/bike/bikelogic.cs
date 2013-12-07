d5516c39670e86b710ce17c16eeb7f7d58bc521e
Tom <tom@Toms-Mac-Pro.local>
2013-12-07 22:44:34 +0100

    networking almost done, only one bug left to fix

--- OculusTron/Assets/prefabs/bike/bikelogic.cs
+++ OculusTron/Assets/prefabs/bike/bikelogic.cs
@@ -5,16 +5,16 @@ public class bikelogic : MonoBehaviour {
 	
 	private GameObject bike1;
 	public Material wall1Material;
+	public GameObject wallPrefab;
 	
 	BikeInputController inputController;
 	int lastDirectionIndex = -1;
 	Vector3 lastCornerPos;
 	GameObject lastCreatedWall;
 	GameObject wallcontainer;
-	
 	float wallHeight = 0.6f;
 	float wallWidth = 0.05f;
-	
+	int wallnumber = 0;
 	
 	// Use this for initialization
 	void Start () {
@@ -34,6 +34,9 @@ public class bikelogic : MonoBehaviour {
 				//make sure wall is closed in the corners
 				extendWall(lastCreatedWall, lastCornerPos, inputController.getLastCorner());
 			}
+			if(networkView.isMine){
+				networkView.RPC("createWall", RPCMode.All, inputController.getLastCorner(), inputController.getDirectionIndex());
+			}
 			lastCreatedWall = createWall(inputController.getLastCorner(), inputController.getDirectionIndex());	
 			lastCornerPos = inputController.getLastCorner();
 			lastDirectionIndex = inputController.getDirectionIndex();
@@ -47,20 +50,19 @@ public class bikelogic : MonoBehaviour {
 		return b1Trans.position;
 	}
 	
+	[RPC]
 	GameObject createWall(Vector3 start, int direction){
-		//Network.Instantiate("wall", start
-		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		wall.transform.Rotate(new Vector3(0, direction*90, 0));
+		GameObject wall = Network.Instantiate(wallPrefab, start, Quaternion.Euler(new Vector3(0, direction*90, 0)), 0) as GameObject;
 		//set matrial
-		MeshRenderer renderer = wall.GetComponent<MeshRenderer>();
-		renderer.material = wall1Material;
+		//MeshRenderer renderer = wall.GetComponent<MeshRenderer>();
+		//renderer.material = wall1Material;
 		
 		//add wall to bike wall container
 		wall.transform.parent = wallcontainer.transform;
-		MeshCollider collider = wall.AddComponent<MeshCollider>();
-		collider.isTrigger = true;
+		//MeshCollider collider = wall.AddComponent<MeshCollider>();
+		//collider.isTrigger = true;
 		
-		wall.name = "bike1_wall";
+		wall.name = "bike_wall"+wallnumber;
 		return wall;
 	}
 	
