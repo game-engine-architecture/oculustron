b714217a58b8e9274026a0e69b3c3f00aa265c86
Tom <tom@Toms-Mac-Pro.local>
2014-01-28 19:00:40 +0100

    added music, better scoreboard, better lighting

---
+++ OculusTron/Assets/MusicController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+	
+	GameStateManager gameState;
+	AudioSource gameMusic;
+	
+	void Start () {
+		this.gameState = gameState = GameObject.Find("GameState").GetComponent<GameStateManager>();
+		this.gameMusic = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameState.isState(GameStateManager.GamesState.MENU)){
+			if(!this.gameMusic.isPlaying){
+				this.gameMusic.volume = 1.0f;
+				this.gameMusic.Play();	
+			}
+		} else {
+			if(this.gameMusic.isPlaying){
+				// make it quiet
+				this.gameMusic.volume -= 0.5f * Time.deltaTime;
+				if(this.gameMusic.volume < 0.01f){
+					this.gameMusic.Stop();
+				}
+			}
+		}
+	}
+}
